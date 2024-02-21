a8b733cb1ff4c9f5c9f53575a61de4e35dd51303
Filipe-souza2 <filipe.pipe.souza@hotmail.com>
2024-02-21 17:44:40 -0300

    desafio concluido

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log("ola")
+class heroi{
+    constructor(nome, idade, tipo) {
+        this.nome = nome
+        this.idade = idade
+        this.tipo = tipo
+    }
+
+    atacar() {
+        if(this.tipo === "mago") {
+            console.log("O " + this.tipo + " atacou usando magia")
+        } else if (this.tipo === "guereiro") {
+            console.log("O " + this.tipo + " atacou usando espada")
+        }else if (this.tipo === "monge") {
+            console.log("O " + this.tipo + " atacou usando artes marciais")
+        }else if (this.tipo === "ninja") {
+            console.log("O " + this.tipo + " atacou usando shuriken")
+        }
+    }
+
+}
+
+let meuNinja = new heroi("NinjaFilipe", 24, "ninja")
+let meuMago = new heroi("MagoFilipe", 24, "mago")
+let meuGuereiro = new heroi("GuerreiroFilipe", 24, "guereiro")
+let meuMonge = new heroi("MongeFilipe", 24, "monge")
+
+meuNinja.atacar()
+meuMago.atacar()
+meuGuereiro.atacar()
+meuMonge.atacar()
